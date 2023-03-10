5116213a048c4e0b6d99b110861fef86b9c2d833
beq <blakebequette@gmail.com>
2023-03-10 07:52:20 -0800

    add BSTreeNode class

--- BST.ts
+++ BST.ts
@@ -12,10 +12,19 @@
  * }
  */
 
+class TreeNode {
+  val: number
+     left: TreeNode | null
+     right: TreeNode | null
+     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+         this.val = (val===undefined ? 0 : val)
+         this.left = (left===undefined ? null : left)
+         this.right = (right===undefined ? null : right)
+     }
+}
+
+
 function sortedArrayToBST(nums: number[]): TreeNode | null {
-    // if (nums.length === 0) return new TreeNode(null, null, null)
-    // if (nums.length === 1) return new TreeNode(nums[0], null, null)
-    
     function inner(lo, hi){
         if (lo > hi) return null
         if (hi === lo) return new TreeNode(nums[lo], null, null)
